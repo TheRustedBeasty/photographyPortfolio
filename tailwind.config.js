d962bd909190388de41e7c37947aca71ea965d74
TheRustedBeasty <100879190+TheRustedBeasty@users.noreply.github.com>
2024-05-15 11:01:43 +1000

    idk

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+    },
+    colors: {
+      'background': '#F6F2EE',
+      'black': '#000000',
+      'white': '#FFFFFF'
+    },
+    fontFamily: {
+      'sans': ['sans-serif'],
   },
   plugins: [],
 }
-
+}
