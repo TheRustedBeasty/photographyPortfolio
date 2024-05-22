5429e18d582bcf08dcf4a2aca82d886d63d17f08
TheRustedBeasty <100879190+TheRustedBeasty@users.noreply.github.com>
2024-05-22 10:10:40 +1000

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,22 @@ function App() {
         <div className='header'>PHOTOGRAPHY</div>
         <div className='subHeader text-right pb-10'>First<br />LastName</div>
       </div>
-      <div className='grid grid-cols-4 gap-3'>
-        <div className='relative'>
-          <img className='absolute hover:opacity-50 transition duration-500' src="src/assets/photo01.png" alt="photo01"/>
-          <div className='label flex h-full justify-center items-center'>STREET</div>
+      <div className='grid grid-cols-4 grid-rows-1 gap-3'>
+        <div className='relative hover:opacity-50 transistion duration-500'>
+          <img className='h-full w-full hover:opacity-50 transition duration-500' src="src/assets/photo01.png" alt="photo01"/>
+          <div className='label absolute transition opacity-0 hover:opacity-100 duration-500 inset-0 flex h-full justify-center items-center'>STREET</div>
         </div>
         <div className='relative'>
-          <img className='absolute hover:opacity-50 transition duration-500' src="src/assets/photo01.png" alt="photo01"/>
-          <div className='label flex h-full justify-center items-center'>STREET</div>
+          <img className='h-full w-full hover:opacity-50 transition duration-500' src="src/assets/photo01.png" alt="photo01"/>
+          <div className='label absolute transition opacity-0 hover:opacity-100 duration-500 inset-0 flex h-full justify-center items-center'>STREET</div>
         </div>
         <div className='relative'>
-          <img className='absolute hover:opacity-50 transition duration-500' src="src/assets/photo01.png" alt="photo01"/>
-          <div className='label flex h-full justify-center items-center'>STREET</div>
+          <img className='h-full w-full hover:opacity-50 transition duration-500' src="src/assets/photo01.png" alt="photo01"/>
+          <div className='label absolute transition opacity-0 hover:opacity-100 duration-500 inset-0 flex h-full justify-center items-center'>STREET</div>
         </div>
         <div className='relative'>
-          <img className='hover:opacity-50 transition duration-500' src="src/assets/photo01.png" alt="photo01"/>
-          <div className='label flex h-full justify-center items-center'>STREET</div>
+          <img className='h-full w-full hover:opacity-50 transition duration-500' src="src/assets/photo01.png" alt="photo01"/>
+          <div className='label absolute transition opacity-0 hover:opacity-100 duration-500 inset-0 flex h-full justify-center items-center'>STREET</div>
         </div>
       </div>
     </div>
